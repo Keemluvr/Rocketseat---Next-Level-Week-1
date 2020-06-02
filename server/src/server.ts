847a8a524270edba62aae62bd6c7a0c4fca1fd88
Keila <keiladaianefernandes24@gmail.com>
2020-06-02 18:56:31 -0300

    :card_file_box: connected to the database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 const port = 3333
 
-app.get('/users', (req, res) => {
-    res.send('HELLO')
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(port)
-console.log(`localhost:${port}/Users`)
+console.log(`----- localhost:${port}/ -----`)
