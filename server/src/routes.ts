bc840b25574d85c550db2d406630540006864056
Keila <keiladaianefernandes24@gmail.com>
2020-06-02 20:48:51 -0300

    :card_file_box: completed item listing functionality

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,24 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
+
+
+/**
+ * Retorna um ou mais items
+*/
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*')
+    // Adicionando a url da imagem
+    const serializedtems = items.map(item => {
+        return { 
+            name: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+         }
+    })
+
+    return res.json(serializedtems)
+})
 
 
 
