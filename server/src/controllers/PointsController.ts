88ce5235c63f008757039577a00760143d6af179
Keila <keiladaianefernandes24@gmail.com>
2020-06-02 23:17:54 -0300

    :card_file_box: filter collection point listing functionality completed

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,46 @@ import { Request, Response } from 'express'
 import knex from '../database/connection'
 
 export default class PointsController {
+
+    /**
+     * Lista os pontos atraves de um filtro
+     */
+    async index (req: Request, res: Response) {
+        const { city, uf, items} = req.query
+        // Converte a string em array
+        const parsedItems = String(items)
+            .split(',')
+            .map( item => Number(item.trim()) )
+        // Retorna os pontos de coleta através do ID dos itens pesquisados
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*')
+
+        return res.json(points)
+    }
+
+    /**
+     * Lista um ponto específico de coleta
+     */
+    async show (req: Request, res: Response) {
+        const { id } = req.params
+        // Busca do id  no banco de dados
+        const point = await knex('points').where('id', id).first()
+        // Caso o id nãos eja encontrado, retorna um erro
+        if(!point) return res.status(400).json({ message: 'Point not found.'})
+        // Retorna o nome dos itens através do seu id
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id)
+            .select('items.title')
+
+        return res.json({ point, items })
+    }
+
     /**
      * Cadastra pontos de coleta
      */
@@ -42,28 +82,13 @@ export default class PointsController {
         // Relaciona um ponto aos seus items de coleta
         await trx('point_items').insert(pointItems)
     
+        // Realiza de vez todas as transações
+        await trx.commit()
+
         return res.json({ 
             id: point_id,
             ...point
         })
-    }
-
-    /**
-     * Lista um ponto específico de coleta
-     */
-    async show (req: Request, res: Response) {
-        const { id } = req.params
-        // Busca do id  no banco de dados
-        const point = await knex('points').where('id', id).first()
-        // Caso o id nãos eja encontrado, retorna um erro
-        if(!point) return res.status(400).json({ message: 'Point not found.'})
-        // Retorna o nome dos itens através do seu id
-        const items = await knex('items')
-            .join('point_items', 'items.id', '=', 'point_items.item_id')
-            .where('point_items.point_id', id)
-            .select('items.title')
-
-        return res.json({ point, items })
     }
 
 }
