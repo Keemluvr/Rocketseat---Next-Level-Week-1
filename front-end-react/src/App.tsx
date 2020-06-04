6be2788531d2d5198b8c11c90e22e9e53d43a1d3
Keila <keiladaianefernandes24@gmail.com>
2020-06-04 00:55:16 -0300

    :truck: renaming files

---
+++ front-end-react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+// Pages
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
