0208179f430f7be758237dc8dd0886459ec7b37a
Keila <keiladaianefernandes24@gmail.com>
2020-06-05 21:00:43 -0300

    ✨ upload images completed

--- front-end-react/src/pages/CreatePoint/index.tsx
+++ front-end-react/src/pages/CreatePoint/index.tsx
@@ -6,9 +6,12 @@ import { LeafletMouseEvent } from "leaflet";
 import axios from "axios";
 import api from "../../services/api";
 
+import DropZone from '../../components/DropZone'
+
 import "./styles.css";
 
 import logo from "../../assets/logo.svg";
+
 
 interface Item {
   id: number;
@@ -34,6 +37,7 @@ const CreatePoint = () => {
     email: '',
     whatsapp: '',
   })
+  const [selectedFile, setSelectedFile] = useState<File>()
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedItems, setSelectedItems] = useState<number[]>([])
@@ -123,17 +127,17 @@ const CreatePoint = () => {
     const city = selectedCity
     const [latitude, longitude] = selectedPosition
     const items = selectedItems
-
-    const data = {
-        name,
-        email,
-        whatsapp,
-        uf,
-        city,
-        latitude,
-        longitude,
-        items
-    }
+        
+    const data = new FormData() 
+    data.append('name', name)
+    data.append('email', email)
+    data.append('whatsapp', whatsapp)
+    data.append('uf', uf)
+    data.append('city', city)
+    data.append('latitude', String(latitude))
+    data.append('longitude', String(longitude))
+    data.append('items', items.join(','))
+    if(selectedFile) data.append('image', selectedFile)
 
     await api.post('/points', data)
         .then( res => {
@@ -164,6 +168,8 @@ const CreatePoint = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <DropZone onFileUploaded={ setSelectedFile } />
 
         <fieldset>
           <legend>
