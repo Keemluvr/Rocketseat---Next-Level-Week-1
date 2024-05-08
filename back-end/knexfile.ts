e151e841c60be33b038d5cc35fe535dd5bb4795e
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-05-07 21:22:33 -0300

    :wrench: update environments to deploy

--- back-end/knexfile.ts
+++ back-end/knexfile.ts
@@ -1,15 +1,42 @@
-import path from 'path'
+import path from "path";
 
 module.exports = {
-    client: 'sqlite3',
+  development: {
+    client: "sqlite3",
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+      filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
     },
     migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
     },
     seeds: {
-        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
     },
     useNullAsDefault: true,
-}
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
+    },
+  },
+};
